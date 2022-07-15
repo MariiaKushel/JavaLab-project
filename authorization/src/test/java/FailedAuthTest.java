84a34a4b244f7d560fd704bff4164a78eb9fd1ae
Mariia Kushel <mariia_kushel@epam.com>
2022-07-15 14:39:57 +0300

    Diactivation failed test

--- authorization/src/test/java/FailedAuthTest.java
+++ authorization/src/test/java/FailedAuthTest.java
@@ -1,9 +1,8 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 public class FailedAuthTest {
 
-    @Test
+    //@Test
     public void authTestShouldBeFailed(){
         Assertions.assertTrue(false);
     }
